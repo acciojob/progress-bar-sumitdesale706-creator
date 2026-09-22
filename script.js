9bfcdfaa73725b7e758d44462fef9950dc7be2d3
Sumit Desale <sumitdesale706@gmail.com>
2026-09-23 00:31:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+var currC = 0;
+
+let progressBar = document.querySelector(".bar");
+let circles = document.querySelectorAll(".circle");
+
+let next = document.querySelector("#next");
+let previous = document.querySelector("#prev");
+
+previous.disabled = true;
+
+next.addEventListener("click", () => {
+	  previous.disabled = false;
+	  currC = currC + 1;;
+		 for(let i = 0; i < 5; i++){
+		    if(i + 1 <= currC){
+				 circles[i].classList.add("active");
+			}else{
+				 circles[i].classList.remove("active");
+			}
+
+			if(currC == 5){
+				 currC == 0;
+		         next.disabled = true;
+		         previous.disabled = false;
+			}
+		 }
+});
+
+previous.addEventListener("click", () => {
+	     circles[currC - 1].classList.remove("active");
+	     currC = currC - 1;
+
+	     if(currC == 0){
+			   next.disabled = false;
+		       previous.disabled = true;
+		 }
+});
+
